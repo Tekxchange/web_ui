c404abb1c862cde5a120f458548435a212cb18fb
Devin Bidwell <dbidwell94@biddydev.com>
2023-01-12 03:03:06 -0800

    Fix No color button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,10 +10,10 @@ export enum ButtonColor {
 
 const buttonColorMap: Record<ButtonColor, string> = {
   [ButtonColor.None]: "bg-transparent hover:bg-gray-100 text-blue-700 hover:shadow-none",
-  [ButtonColor.Red]: "bg-red-400 hover:bg-red-300 text-black",
-  [ButtonColor.Green]: "bg-green-400 hover:bg-green-300 text-black",
-  [ButtonColor.Blue]: "bg-blue-300 hover:bg-blue-200 text-black",
-  [ButtonColor.Gold]: "bg-yellow-400 hover:bg-yellow-300 text-black",
+  [ButtonColor.Red]: "bg-red-400 hover:bg-red-300 text-black hover:shadow-black",
+  [ButtonColor.Green]: "bg-green-400 hover:bg-green-300 text-black hover:shadow-black",
+  [ButtonColor.Blue]: "bg-blue-300 hover:bg-blue-200 text-black hover:shadow-black",
+  [ButtonColor.Gold]: "bg-yellow-400 hover:bg-yellow-300 text-black hover:shadow-black",
 };
 
 interface IButtonProps
@@ -36,7 +36,7 @@ const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
         className={`
         ${
           buttonColorMap[buttonColor ?? ButtonColor.None]
-        } py-1 px-2 rounded-md border-transparent hover:shadow-sm hover:shadow-black active:shadow-black transition-colors box-border border text-lg active:shadow-lg-inner
+        } py-1 px-2 rounded-md border-transparent hover:shadow-sm active:shadow-black transition-colors box-border border text-lg active:shadow-lg-inner
         `.trim()}
       >
         <>
