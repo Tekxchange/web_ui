1a432eb191bf2e1bc3f9b4fd97a9f841adb7ec66
Devin Bidwell <dbidwell94@biddydev.com>
2023-01-12 22:04:05 -0800

    classes helper function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Easily clean up the CSS className field with this helper function
+ * @param strings A list of classes to apply
+ * @returns A single string joined with a space and all empty classes removed
+ */
 export function classes(...strings: string[]) {
-  return strings.join(" ");
+  return strings
+    .map((c) => c.trim())
+    .filter((c) => Boolean(c))
+    .join(" ");
 }
