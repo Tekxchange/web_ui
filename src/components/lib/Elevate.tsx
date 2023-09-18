6d0425ea295dfbb2e9d9ee8835e5b0a1eb43a4b2
Devin Bidwell <dbidwell94@biddydev.com>
2023-09-17 21:27:28 -0700

    remove console.log in Elevate.tsx

--- src/components/lib/Elevate.tsx
+++ src/components/lib/Elevate.tsx
@@ -27,7 +27,6 @@ export default function Elevate(props: Props) {
       if (React.isValidElement(child)) {
         let className = (child.props["className"] as string) ?? "";
         className = c`${className} ${elevationToClassname[elevation]}`;
-        console.log(className);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         return React.cloneElement<any>(child, { className });
       }
