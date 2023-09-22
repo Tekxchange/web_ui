f7d8799334ef47c01429c6e48369efa931ff489b
Devin Bidwell <dbidwell94@biddydev.com>
2023-09-21 22:34:59 -0700

    working search inputs

--- src/pages/app.page/resultsBar/Filter.tsx
+++ src/pages/app.page/resultsBar/Filter.tsx
@@ -1,7 +1,7 @@
 import Elevate, { Elevation } from "@components/lib/Elevate";
 import { useAppDispatch, useAppSelector } from "@state/index";
 import PriceFilter from "./PriceFilter";
-import { Option } from "@utils/option";
+import { Option, none, some } from "@utils/option";
 import Input from "@components/Input";
 import { updateSearchResults } from "@state/search";
 
@@ -9,13 +9,26 @@ export default function Filter() {
   const { filter } = useAppSelector((state) => state.search);
   const dispatch = useAppDispatch();
 
-  async function onPriceUpdated(min: Option<number>, max: Option<number>) {}
+  async function onPriceUpdated(min: Option<number>, max: Option<number>) {
+    dispatch(updateSearchResults({ priceLow: min, priceHigh: max }));
+  }
 
   return (
     <Elevate elevation={Elevation.High}>
       <div className={"flex items-center px-2 md:px-10 gap-2 md:gap-5 w-fit"}>
         <PriceFilter onSubmit={onPriceUpdated} />
-        <Input label={"Search Text"} id={"searchText"} name={"query"} />
+        <Input
+          label={"Search Text"}
+          id={"searchText"}
+          name={"query"}
+          value={filter.query.raw}
+          onChange={({ target: { value } }) => {
+            let toSet: Option<string>;
+            if (!value) toSet = none();
+            else toSet = some(value);
+            dispatch(updateSearchResults({ query: toSet }));
+          }}
+        />
         <Input
           type={"number"}
           label={`Search Radius (${filter.radiusUnit})`}
